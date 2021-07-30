dee508cf4295de2e5d2e8f833a56521952a6d772
Stephanie Scott <stephanie.scott.1719@gmail.com>
2021-07-30 13:17:07 -0400

    third update

--- MoneyConversion.java
+++ MoneyConversion.java
@@ -20,7 +20,6 @@ public class MoneyConversion
 {
     //properties of class
     private static Scanner scanner = new Scanner (System.in);
-    //private static DecimalFormat percentPattern = new DecimalFormat("0.0#%");
     //private static DecimalFormat pricePattern = new DecimalFormat("$0.00");
 
     public static void main(String[] args) 
@@ -29,12 +28,13 @@ public class MoneyConversion
         {
             //prompt the user for an integer representing how many
             //quarters, dimes, nickles, and pennies they have (4 integers total)
-            int amountOfQuarters = getAmountOfQuarters();
-            int amountOfDimes = getAmountOfDimes();
-            int amountOfNickles = getAmountOfNickles();
-            int amountOfPennies = getAmountOfPennies();
+            double amountOfQuarters = getAmountOfQuarters();
+            double amountOfDimes = getAmountOfDimes();
+            double amountOfNickles = getAmountOfNickles();
+            double amountOfPennies = getAmountOfPennies();
 
             //convert coin amounts to dollars
+            printAmountOfMoney(amountOfQuarters, amountOfDimes, amountOfNickles, amountOfPennies);
 
             //output in dollar notation
 
@@ -50,31 +50,32 @@ public class MoneyConversion
         }
     }
 
-    public static int getAmountOfQuarters()
+    public static double getAmountOfQuarters()
     {
         log("How many quarters do you have?");
-        int amountOfQuarters = scanner.nextInt();
-        return amountOfQuarters;
+        double amountOfQuarters = scanner.nextInt();
+        double quarterAmount = 0.25 x amountOfQuarters;
+        return quarterAmount;
     }
 
-    public static int getAmountOfDimes()
+    public static double getAmountOfDimes()
     {
         log("How many dimes do you have?");
-        int amountOfDimes = scanner.nextInt();
+        double amountOfDimes = scanner.nextInt();
         return amountOfDimes;
     }
 
-    public static int getAmountOfNickles()
+    public static double getAmountOfNickles()
     {
         log("How many nickles do you have?");
-        int amountOfNickles = scanner.nextInt();
+        double amountOfNickles = scanner.nextInt();
         return amountOfNickles;
     }
 
-    public static int getAmountOfPennies()
+    public static double getAmountOfPennies()
     {
         log("How many pennies do you have?");
-        int amountOfPennies = scanner.nextInt();
+        double amountOfPennies = scanner.nextInt();
         return amountOfPennies;
     }
 
