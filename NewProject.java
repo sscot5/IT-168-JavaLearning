81c25dcbfcb5ef434711bcd37d77e99f070d48fc
Stephanie Scott <stephanie.scott.1719@gmail.com>
2021-08-06 08:14:46 -0500

    new program

---
+++ NewProject.java
@@ -0,0 +1,58 @@
+/**
+ * Author: Stephanie Scott
+ * 
+ * Name: 
+ * 
+ * Date: 8/3/21
+ * 
+ * Ask user to provide their three favorite foods
+ */
+
+import java.util.Scanner;
+
+public class NewProject {
+    private static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String [] args){
+        try {
+            // declaring a foods variable
+            String[] foods;
+
+            // initializing an array with length 3
+            int numberOfFoods = 3;
+            
+            foods = new String[numberOfFoods];
+
+            //prompt user for their favorite foods
+            for (int i = 0; i < numberOfFoods; i++) {
+                foods[i] = getFavFoods(i);
+            }
+
+            for (String favFood : foods) {
+                printFavFood(favFood);
+            }
+
+
+        }
+        catch (Exception nope){
+            log("Nope. Try again.");
+        }
+    }
+
+    public static String getFavFoods(int num){
+        log("What is your favorite food #" + (++num));
+        String favFood = scanner.nextLine();
+        return favFood;
+    }
+
+    public static void printFavFood(String favFood) {
+        log("Favorite food: " + favFood);
+    }
+
+    public static void log(String msg){
+        System.out.println(msg);
+    }
+
+
+}
+
