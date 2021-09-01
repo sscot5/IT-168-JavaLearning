4245f9096dd3617cced7430ce6215f83fb4d8301
Stephanie Scott <stephanie.scott.1719@gmail.com>
2021-09-01 11:21:34 -0500

    miles per gallon

--- ClassWork.java
+++ ClassWork.java
@@ -1,31 +1,20 @@
-public class ClassWork {
-    public static void main(String[] args) {
-        //declaration
-    Student stu1
+// public class ClassWork {
+//     public static void main(String[] args) {
+//         //declaration
+//     Student stu1
 
-        //instantiation
-    Student stu1 = new Student();
-    Monster theEnemy = new Monster();
-    Employee john = new Employee();
+//         //instantiation
+//     Student stu1 = new Student();
+//     Monster theEnemy = new Monster();
+//     Employee john = new Employee();
     
-        //instantiation declaration and assign variable (declare and create)
-    Student stu2 = new Student("Anne Hawkins");
+//         //instantiation declaration and assign variable (declare and create)
+//     Student stu2 = new Student("Anne Hawkins");
 
 
-    //Examples done in class
-    String str = new String("Programming in Java");
-    SimpleDate defaultDate = new SimpleDate();
-    SimpleDate today = new SimpleDate(8,27, 21);
-    SavingsAccount myAccount = new SavingsAccount(1000);
+//     //Examples done in class
+//     String str = new String("Programming in Java");
+//     SimpleDate defaultDate = new SimpleDate();
+//     SimpleDate today = new SimpleDate(8,27, 21);
+//     SavingsAccount myAccount = new SavingsAccount(1000);
 
-
-    final double SALES_TAX = 0.0625;
-    final double TIP = 0.2;
-    // scanner and ask for the amount of the bill prior to tax
-    // calculate tax
-    // add bill + tax (total)
-    // calculate tip off of total
-    // print bill amount, tax amount, tip amount, and total all with labels
-    
-    }
-}
--- MilesPerGallon.java
+++ MilesPerGallon.java
@@ -12,7 +12,21 @@ import java.util.Scanner;
 
 
 public class MilesPerGallon {
+    public static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        
+
+        //ask for the number of gallons used on trip
+        System.out.print("How many gallons of gas did you use on your trip?  ");
+        int gallonsGasUsed = scanner.nextInt();
+
+        //ask for the number of miles driven on trip
+        System.out.print("How many miles did you travel on your trip?  ");
+        int milesTraveled = scanner.nextInt();
+
+        //calculate miles per gallon
+        double milesPerGallon = (double) milesTraveled / gallonsGasUsed;
+
+        //print miles per gallon
+        System.out.println("The miles per gallon for your trip is " + milesPerGallon);
     }
 }
