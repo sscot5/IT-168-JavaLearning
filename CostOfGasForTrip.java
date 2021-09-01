41b33b19053cf3f486359563b7a92e22a5a7d986
Stephanie Scott <stephanie.scott.1719@gmail.com>
2021-09-01 16:10:56 -0500

    new program Cost of gas for trip

---
+++ CostOfGasForTrip.java
@@ -0,0 +1,36 @@
+/*
+ * Author: Stephanie Scott
+ * Date: 9/1/21
+ */
+
+/**
+ * Description: A program to calculate cost of gas for trip
+  */
+
+  // imports
+  import java.util.Scanner;
+  import java.text.DecimalFormat;
+
+public class CostOfGasForTrip {
+    public static Scanner scanner = new Scanner(System.in);
+    public static DecimalFormat pricePattern = new DecimalFormat("$0.00");
+    public static void main(String[] args) {
+        //ask for total miles for trip
+        System.out.print("What is the total number of miles for your trip?  ");
+        double totalMiles = scanner.nextDouble();
+
+        //ask how many mpg does your car use
+        System.out.print("How many miles per gallon does your vehichle average?  ");
+        double mpg = scanner.nextDouble();
+
+        //ask what is the current price of gas
+        System.out.print("What is the current price of gas?  ");
+        double gasPrice = scanner.nextDouble();
+
+        //calculate cost = miles of trip / mpg * current price of gas
+        double costOfGasForTrip = (totalMiles / mpg) * gasPrice;
+
+        //print cost of trip
+        System.out.println("The total cost of your trip in gas is " + pricePattern.format(costOfGasForTrip));
+    }
+}
