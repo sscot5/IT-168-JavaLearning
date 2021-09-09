9fae4e55ae39a21e55f40ab34fc4d0bcbc8f6cab
Stephanie Scott <stephanie.scott.1719@gmail.com>
2021-09-09 14:58:52 -0500

    MD notes

---
+++ OOP.md
@@ -0,0 +1,30 @@
+Classes information
+
+properties or attributes --> what a class stores
+
+methods --> how we mutate the properties or attributes stored
+
+
+= new ClassName is an instance creating an object that can
+be mutated by using instance methods
+
+Employee john = new Employee("John");
+
+john is a identifier that references the object
+new Employee is a constructor method
+
+static methods --> input all the data into the method
+               --> it does not store any information
+               -->The math class is a static class because none of the methods are instance method.
+instance methods --> keeps track of information that the class is not aware of
+
+variable --> holds information
+instance --> value that the variable is referencing 
+
+EXAMPLE:
+Square is four equal sides --> instructions to make a square (Class)
+a--> draw one square b-->second square
+a and b are variable that identifes my instances (drawing) of a square
+The class tells the computer how much space to save for our data
+The constructor is what is building our squares for us and need to pass the value
+that tells the computer how large to build our square
--- Payroll.java
+++ Payroll.java
@@ -1,16 +1,46 @@
-/*
- * Created on 9/9/21
- * By Stephanie Scott
- */
-
-/**
- * Employee class stores information about an hourly employee
+/**Created on 9/9/2020
  * 
- * @author Stephanie Scott
+ * Using the given Employee class and creating a payroll from it
  */
 
- public class Payroll{
-     public static void main(String[] args) {
-         
-     }
- }
+//imports
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class Payroll {
+    public static Scanner scanner = new Scanner(System.in);
+    public static DecimalFormat pricePattern = new DecimalFormat("$0.00");
+    public static void main(String[] args) {
+        Employee employee1 = new Employee("Smith", "John", 15); //instance of the Employee class
+        // yellow Employee is an object
+
+        System.out.print("Enter employee's first name: ");
+        String fName = scanner.nextLine();
+        System.out.print("Enter employee's last name: ");
+        String lName = scanner.nextLine();
+        System.out.print("Enter the employee's pay rate: ");
+        Double rate = scanner.nextDouble();
+
+        Employee employee2 = new Employee(lName, fName, rate);
+
+        employee1.print();
+        employee2.print();
+
+        System.out.println(employee1.getName() + " recieved " + pricePattern.format(employee1.calculatePay(42)));
+        System.out.println(employee2.getName() + " recieved " + pricePattern.format(employee2.calculatePay(40)));
+
+        System.out.print("What precentage raise does John Smith get? ");
+        double percentRaise1 = scanner.nextDouble();
+        System.out.print("What precentage raise does " + fName + " " + lName + "get? ");
+        double percentRaise2 = scanner.nextDouble();
+
+        employee1.giveRaise(percentRaise1);
+        employee2.giveRaise(percentRaise2);
+
+        employee1.printPayrollLine(42);
+        employee2.printPayrollLine(40);
+
+        employee1.print();
+        employee2.print();
+    }
+}
