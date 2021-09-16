14af13857d57576efb38fa2e0a53ffb90abb9435
Stephanie Scott <stephanie.scott.1719@gmail.com>
2021-09-16 17:19:09 -0500

    loop condition program

--- BinomialProb.java
+++ BinomialProb.java
@@ -5,13 +5,34 @@
 
 /**
  * Description: A program that calculates the mean and standard deviations for binmial probability.
- * mean = n * q;
- * standard deviation = square root (n * p * (1-p));
+ * mean = n * p
+ * standard deviation = square root (n * p * (1-p))
  * 
+ * n is the total number of trials
+ * p is the probability of success
  */
 
  // imports
+import java.util.Scanner;
 
 public class BinomialProb {
-    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // ask for the total number of trials, the probability of success, and the number of successes in n trials
+        System.out.print("Enter the total number of trials: ");
+        int n = scanner.nextInt();
+        System.out.print("Enter the probability of success: ");
+        double p = scanner.nextDouble();
+       
+
+        // calculate mean (n * p)
+        double mean = n * p;
+        System.out.println("The mean is: " + mean);
+
+        // calculate the standard deviation = square root (n * p * (1-p))
+        double a = n * p * (1 - p);
+        double standardDeviation = Math.sqrt(a);
+        System.out.println("The standard deviation is: " + standardDeviation);
+    }
 }
---
+++ LoopCondition.java
@@ -0,0 +1,62 @@
+/*
+ * Author: Stephanie Scott
+ * Date: 9/16/21
+ */
+
+/**
+ * Description: A program that takes an order from a menu and calculates the cost of the order.
+ */
+
+ // imports
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+public class LoopCondition {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        DecimalFormat pricePattern = new DecimalFormat("$0.00");
+
+        String menu = "\tC Cheeseburger: $7.49";
+                menu += "\n\tH Hot dog: $6.99";
+                menu += "\n\tL Lemonade: $2.50";
+                menu += "\n\tT Iced tea: $2.75";
+        
+        double orderCost = 0;
+
+        System.out.println("Welcome to Bonnie's Burgers. \n Select from our menu.");
+        System.out.println(menu);
+        System.out.print("Order an item or \"S\" to stop: ");
+        char option = scanner.next().charAt(0);
+
+        while (option != 'S' && option != 's') {
+            switch (option) {
+                case 'c':
+                case 'C':
+                    System.out.print("Cheeseburger ordered.");
+                    orderCost += 7.49;
+                    break;
+                case 'h':
+                case 'H':
+                    System.out.print("Hot dog ordered.");
+                    orderCost += 6.99;
+                    break;
+                case 'l':
+                case 'L':
+                    System.out.print("Lemonade ordered.");
+                    orderCost += 2.50;
+                    break;
+                case 't':
+                case 'T':
+                    System.out.print("Iced tea ordered.");
+                    orderCost += 2.75;
+                    break;
+                default:
+                    System.out.println("Unrecongnized menu item.");
+            }
+            System.out.println("\nSubtotal: " + pricePattern.format(orderCost));
+            System.out.print("\nOrder another item or \"S\" to stop: ");
+            option = scanner.next().charAt(0);
+        }
+        System.out.println("\nTotal order cost is: " + pricePattern.format(orderCost));
+    }    
+}
