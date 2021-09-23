1c2bd2f914d65187ff8c31a8a50bd02e101677b3
Stephanie Scott <stephanie.scott.1719@gmail.com>
2021-09-22 21:09:08 -0500

    updates for 9/22 end of day

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -11,23 +11,26 @@
 import java.util.Scanner;
 
 public class PrimeNumber {
-    
     public static void main(String[] args) {
-    Scanner scanner = new Scanner(System.in);
-    System.out.print("Enter a positive number: ");
-    int num = scanner.nextInt();
-    scanner.close();
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a positive number: ");
+        long num = scanner.nextLong();
+        scanner.close();
 
-    boolean prime = true;
-    for (int i = 2; i < num; i++){
-        if (num % i == 0){
-            prime = false;
+        boolean prime = true;
+        int endNum = (int)Math.sqrt(num);
+    
+        for (long i = 2; i <= endNum; i++){
+            if (num % i == 0){
+                prime = false;
+                break;
+            }
         }
-    }
-    if (prime && num > 1){
-        System.out.println("The number " + num + " is a prime number.");
-    } else {
-        System.out.println("The number " + num + " is not a prime number.");
-    }
+        
+        if (prime && num > 1){
+            System.out.println("The number " + num + " is a prime number.");
+        } else {
+            System.out.println("The number " + num + " is not a prime number.");
+        }
     }
 }
