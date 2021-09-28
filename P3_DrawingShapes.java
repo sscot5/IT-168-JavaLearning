fdbf90ff2732264e3de64f1973f67a70e4b5f29e
Stephanie Scott <stephanie.scott.1719@gmail.com>
2021-09-28 16:59:30 -0500

    program 3 update

--- P3_DrawingShapes.java
+++ P3_DrawingShapes.java
@@ -1,5 +1,5 @@
 /**
- * Description: A program that 
+ * Description: A program that allows the user to draw a rectangle or A shape until they exit the program.
  * @author: Stephanie Scott
  * Date: 9/24/21
  *
@@ -9,65 +9,197 @@
 import java.util.Scanner;
 
 /*
+ * INFO NEEDED:
  * 1 – draw a rectangle shape
  * 2 – draw an A shape
  * 0 - quit
+ * 
+ * ASCII value for numbers 0 - 9 == 48 - 57
  */
 
 public class P3_DrawingShapes {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("Welcome to the drawing application!");
-        System.out.println("\nPlease choose one of the following: \n1 – draw a rectangle shape \n2 – draw an A shape \n0 - quit");
-        System.out.print("Enter your choice: ");
-        int num = scanner.nextInt();
 
-        while (num != 0) {
+        // Validating
+        boolean drawing = true;
+        while (drawing) {
+            System.out.println(
+                    "\nPlease choose one of the following: \n1 – draw a rectangle shape \n2 – draw an A shape \nAt any point enter 0 to quit");
+            System.out.print("\nEnter your choice: ");
+            String input = scanner.nextLine();
 
-            // choice 1 -- rectangle shape
-            if (num == 1) {
-                System.out.println("Let's draw a rectangle!");
-                System.out.print("Enter the number of rows: ");
-                int numRows = scanner.nextInt();
-                System.out.print("Enter the number of columns: ");
-                int numColumns = scanner.nextInt();
-                System.out.print("Enter the character of your choice: ");
-                String character = scanner.next();
-                
-                for (int i = 0; i < numRows; i++){
-                    for (int j = 0; j < numColumns; j++) {
-                        System.out.print(character);
+            if (input.equals("0")) {
+                drawing = false;
+                continue;
+
+            } else if (input.equals("1")) {
+                boolean drawRect = true;
+
+                while (drawRect) {
+                    System.out.println("\nLets draw a rectangle!");
+                    System.out.print("\nEnter the number of rows: ");
+                    String rowsString = scanner.nextLine();
+                    System.out.print("Enter the number of columns: ");
+                    String columnsString = scanner.nextLine();
+                    int rows;
+                    int columns;
+
+                    // TODO: break apart for rows and columns to be greater than zero but less than
+                    // what an int can handle
+                    // validates the user provided an input
+                    if (rowsString.length() == 0 || columnsString.length() == 0) {
+                        System.out.println("Please enter a value for both rows and columns.");
+                        continue;
                     }
-                    System.out.println();
+
+                    // validate rows as an int
+                    boolean rowsIsAnInt = true;
+                    for (int i = 0; i < rowsString.length(); i++) {
+                        if ((int) rowsString.charAt(i) >= 48 && (int) rowsString.charAt(i) <= 57) {
+                            continue;
+                        }
+                        rowsIsAnInt = false;
+                        break;
+                    }
+
+                    // validate columns as an int
+                    boolean columnsIsAnInt = true;
+                    for (int i = 0; i < columnsString.length(); i++) {
+                        if ((int) columnsString.charAt(i) >= 48 && (int) columnsString.charAt(i) <= 57) {
+                            continue;
+                        }
+                        columnsIsAnInt = false;
+                        break;
+                    }
+
+                    // converting the input from a string to an integer and validating that they did
+                    // not enter 0 to quit
+                    if (rowsIsAnInt && columnsIsAnInt) {
+                        rows = Integer.parseInt(rowsString);
+                        columns = Integer.parseInt(columnsString);
+                        if (rows == 0 || columns == 0) {
+                            drawRect = false;
+                            drawing = false;
+                            continue;
+                        }
+                    } else {
+                        System.out.println("Error: invalid input.");
+                        continue;
+                    }
+
+                    // validate a character was entered
+                    String character = "";
+                    while (character.length() == 0) {
+                        System.out.print("Enter a character for the rectangle: ");
+                        character = scanner.nextLine();
+                    }
+
+                    // how to handle inputs like space so it doesn't print a blank rectangle you
+                    // cannot see
+                    // the ascII value 33 to 126 are printable characters
+                    if (character.charAt(0) < 33 || character.charAt(0) > 126) {
+                        System.out.println("\nCharacter input was invalid. Changing to default character.");
+                        character = "-";
+                    } else if (character.charAt(0) == 48) {
+                        drawRect = false;
+                        drawing = false;
+                        continue;
+                    }
+
+                    // drawing the rectangle
+                    for (int i = 0; i < rows; i++) {
+                        for (int j = 0; j < columns; j++) {
+                            System.out.print(character.charAt(0));
+                        }
+                        System.out.println();
+                    }
+                    drawRect = false;
                 }
-            }
 
-            // choice 2 -- A shape
-            if (num == 2){
-                System.out.println("Let's draw an A!");
-                System.out.print("Enter an odd number between 11 and 23 to determine the size of A: "); // needs to be read as a string
-                int oddNum = scanner.nextInt();
+            } else if (input.equals("2")) {
+                boolean drawA = true;
 
-                if (oddNum % 2 == 0 || oddNum < 11 || oddNum > 23) { //FIXME
-                    System.out.print("Please enter an odd number between 11 and 23: ");
-                    oddNum = scanner.nextInt();
-                } else {
-                    // Draw A Shape
+                while (drawA) {
+                    // get input
+                    System.out.println("Let's draw an A!");
+                    System.out.print("Enter an odd number between 11 and 23 to determine the size of A: ");
+                    String widthString = scanner.nextLine();
+                    int width;
+
+                    // validates the user provided an input and that input is only 2 characters long
+                    // to fit the range of 11 - 23
+                    if (widthString.length() != 2) {
+                        System.out.println("Error: Invalid input");
+                        continue;
+                    }
+
+                    // validate input as an int
+                    boolean widthIsAnInt = true;
+                    for (int i = 0; i < widthString.length(); i++) {
+                        if ((int) widthString.charAt(i) >= 48 && (int) widthString.charAt(i) <= 57) {
+                            continue;
+                        }
+                        widthIsAnInt = false;
+                        break;
+                    }
+
+                    // converting the input from a string to an integer and validating that user did
+                    // not enter 0 to quit
+                    if (widthIsAnInt) {
+                        width = Integer.parseInt(widthString);
+                        if (width == 0) {
+                            drawA = false;
+                            drawing = false;
+                            continue;
+                        }
+                    } else {
+                        System.out.println("Error: " + widthString + " is an invalid input.");
+                        continue;
+                    }
+
+                    // once input is an int -- make sure it is an odd integar between 11 and 23
+                    if (width % 2 == 0 || width < 11 || width > 23) {
+                        System.out.println("Error: " + width + " is an invalid input");
+                        continue;
+                    }
+
+                    // draw A
+                    int height = (width + 1) / 2;
+                    int midLine = (height + 1) / 2;
+                    String charPrint = "A";
+                    String fillPrint = " ";
+
+                    for (int row = 0; row < height; row++) {
+                        for (int spaces = 0; spaces < height - row - 1; spaces++) {
+                            System.out.print(fillPrint);
+                        }
+                        System.out.print(charPrint);
+                        if (row > 0) {
+                            String a;
+                            if (row == midLine) {
+                                a = charPrint;
+                            } else {
+                                a = fillPrint;
+                            }
+                            int fill = row * 2 - 1;
+                            for (int i = 0; i < fill; i++) {
+                                System.out.print(a);
+                            }
+                            System.out.print(charPrint);
+                        }
+                        System.out.println();
+                    }
+                    drawA = false;
                 }
-            }  
-        
-            if (num != 0 || num != 1 || num != 2) {
-                System.out.println("Please enter 1 to draw a rectangle, 2 to draw an A, or 0 to quit.");
-                System.out.print("Enter your choice: ");
-                num = scanner.nextInt();
-            }
-        } 
-        
-        scanner.close();
 
-        if (num == 0) {
-            System.out.println("Goodbye!");
+            } else {
+                System.out.println(input + " is invalid.");
+                continue;
+            }
         }
+        scanner.close();
+        System.out.println("You entered a 0. Exiting program. Goodbye");
     }
 }
