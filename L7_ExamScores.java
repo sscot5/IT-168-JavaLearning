0343152edef036c3a53a345d3fba2cbd8d0c7fc5
Stephanie Scott <stephanie.scott.1719@gmail.com>
2021-09-29 05:22:59 -0500

    formatting lab 7

--- L7_ExamScores.java
+++ L7_ExamScores.java
@@ -3,21 +3,18 @@
  * Date: 9/28/21
  */
 
-/**
- * Description: A program that takes test scores from students and determines the max, min, and average of all test scores.
- * Each exam score will be an integer in the range 0 to 100
- * Input will end when the user enters -1 as the input value
- * 
- * After all scores have been read
- * display the number of students who took the exam
- * the minimum score
- * the maximum score
- * the average score (with decimal point)
- * the number of As
- */
-
-// imports
 import java.util.Scanner;
+
+/**
+ * Description: A program that takes test scores from students and determines
+ * the max, min, and average of all test scores. Each exam score will be an
+ * integer in the range 0 to 100 Input will end when the user enters -1 as the
+ * input value
+ * 
+ * After all scores have been read display the number of students who took the
+ * exam the minimum score the maximum score the average score (with decimal
+ * point) the number of As
+ */
 
 public class L7_ExamScores {
     public static void main(String[] args) {
